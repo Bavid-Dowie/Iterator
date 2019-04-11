ce330d7eced25589e1d05b242114b049215a5990
Madeline Rombes <madelinerombes@gmail.com>
2019-04-11 14:53:34 -0400

    miscellaneous

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <ul>
+            <li>© M3J 2019</li>
+            <li>view it on <a href="https://github.com/Bavid-Dowie/Iterator">Github</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
