5e3638c409d7f2a3da500a43d8632ae64cbc54eb
Madeline Rombes <madelinerombes@gmail.com>
2019-04-11 14:47:35 -0400

    create basic article component

---
+++ client/src/components/Article.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Header from './Header'
+
+class Article extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Article Title</h1>
+        <h3>Byline</h3>
+        <p>Lorem ipsum</p> 
+      </div>
+    )
+  }
+}
+
+export default Article
