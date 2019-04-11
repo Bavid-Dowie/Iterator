573a1d102d6c2b90c98f17b0e72da7eab66e4da9
Madeline Rombes <madelinerombes@gmail.com>
2019-04-11 15:49:27 -0400

    basic userprofile styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserProfile from './components/UserProfile'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <UserProfile />
       </div>
     );
   }
