0a97f970dda46c9f94b4cb874c6e8725d3f83d8c
Madeline Rombes <madelinerombes@gmail.com>
2019-04-11 18:54:02 -0400

    import router, set init state

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Article from './components/Article'
+import CreateArticle from './components/CreateArticle'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Homepage from './components/Homepage'
+import UpdateArticle from './components/UpdateArticle'
+import UserProfile from './components/UserProfile'
+
 
 class App extends Component {
+  constructor(props) {
+    this.state = {
+      users: [],
+      articles: []
+    }
+  }
+  
   render() {
     return (
       <div className="App">
