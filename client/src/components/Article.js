c802b676aeb4d8f0c95974355cea26f84b6390d9
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-12 15:00:44 -0400

    add getArticle function, call in componentDidMount

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router'
 import Header from './Header'
+import UpdateArticle from './UpdateArticle';
+
+const url = 'http:localhost:3001/articles/'
 
 class Article extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      article: ""
+    }
+  }
+
+  getArticle(){
+    fetch(`${url}${this.props.match.params.id}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data.article)
+        this.setState({ article: data.article })
+      })
+  }
+
+  componentDidMount(){
+    this.getArticle()
+  }
+
   render() {
     return (
-      <div className="article">
+      <div className="article-page">
         <Header />
-        <h1>Article Title</h1>
-        <h3>Byline</h3>
-        <p>Lorem ipsum</p> 
+       
       </div>
     )
   }
 }
 
-export default Article
+export default withRouter(Article)
