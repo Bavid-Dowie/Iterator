6fb78076e1ab2c8a2dba548223bf805a3c8337fc
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-12 09:45:29 -0400

    add Update User component

---
+++ client/src/components/UpdateUser.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class UpdateUser extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            username: ""
+        }
+    }
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default UpdateUser
