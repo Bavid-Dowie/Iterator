11edfabe56ce89fd7bcde05ddde9d2a9af79ea4f
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-13 19:42:50 -0400

    create Articles component

---
+++ client/src/components/Articles.js
@@ -0,0 +1,13 @@
+import React, { Component } from React
+
+class Articles extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Articles
