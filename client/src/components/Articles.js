5c7ac0d8d9755fae36e04ebe4b8d69494f0674d3
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-13 20:19:38 -0400

    fetch user articles function

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -1,6 +1,23 @@
 import React, { Component } from React
 
+const url = 'https://iterator.herokuapp.com/articles/:userId'
+
 class Articles extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            userArticles: []
+        }
+    }
+
+getUserArticles() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        this.setState({ userArticles: data.articles })
+    })
+}
+
     render(){
         return(
             <div>
