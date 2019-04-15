fcf4ea864a4b5b8b7c9089926cc2da0b9d4cd59b
Jeremy Zimmerman <46272586+Bavid-Dowie@users.noreply.github.com>
2019-04-15 15:19:22 -0400

    Update CreateArticle.js

--- client/src/components/CreateArticle.js
+++ client/src/components/CreateArticle.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const url = `https://iterator.herokuapp.com/articles` 
+const url = `https://iterator.herokuapp.com/articles`
 
 class CreateArticle extends Component {
     constructor(props) {
