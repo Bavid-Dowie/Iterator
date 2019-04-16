921ec1599b214e4b76a4bf624bcd6d3dd2ca06d0
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-04-16 19:27:43 -0400

    Update Articles style

--- client/src/components/AllArticles.js
+++ client/src/components/AllArticles.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Logo from '../images/logo-dark.png'
+import articleTech from '../images/articles_banner.png'
 import { withRouter } from 'react-router-dom';
 import Footer from '../components/Footer'
+
+const getRandomNum = () => {
+    return Math.floor(Math.random() * 206)
+}
 
 const AllArticles = (props) => {
     return (
@@ -10,12 +15,15 @@ const AllArticles = (props) => {
                 <img alt="Iterator logo" src={Logo} />
                 <button className="allarticles__backbtn" onClick={props.history.goBack}>Back to Profile</button>
             </div>
-            <div key={Math.random()}className="allarticles__articles--div">
+            <div className="articles_banner">
+                <img src={articleTech} alt="" className="articles__banner-photo" />
+            </div>
+            <div key={Math.random()} className="allarticles__articles--div">
                 {props.renderAllArticles()}
             </div>
             <Footer />
         </div>
     )
-} 
+}
 
 export default withRouter(AllArticles)
