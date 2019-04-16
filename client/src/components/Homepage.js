a0c93d103b42cd12ed89afa061b685a08cbd6d44
Elizabeth Moy <elizabethtmoy@gmail.com>
2019-04-16 02:05:59 -0400

    add & style icons

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -3,6 +3,9 @@ import {Redirect, Link} from 'react-router-dom'
 import logo from '../images/iterator_logo.png'
 import title from '../images/iterator_description.png'
 import image from '../images/homepage-image.png'
+import laptopIcon from '../images/laptop_icon.png'
+import codeIcon from '../images/code_icon.png'
+import shareIcon from '../images/share_icon.png'
 
 const Homepage = (props) => {
     if (props.loggedin === true) {
@@ -18,9 +21,9 @@ const Homepage = (props) => {
                         await props.handleLoginSubmit()
                     }}>
                         <div className="homepage__login-field">
-                            <label className="homepage__login-label" htmlFor="username">Username </label>
+                            <label className="homepage__login-label" htmlFor="username">username </label>
                             <input 
-                                className="input" 
+                                className="login-input" 
                                 type="text" 
                                 name="username" 
                                 placeholder="username" 
@@ -29,8 +32,8 @@ const Homepage = (props) => {
                             ></input>
                         </div>
                         <div className="homepage__login-field">
-                            <label className="homepage__login-label" htmlFor="password">Password </label>
-                            <input className="input" type="password" name="password" placeholder="password" value={props.userPassword}></input>
+                            <label className="homepage__login-label" htmlFor="password">password </label>
+                            <input className="login-input" type="password" name="password" placeholder="password" value={props.userPassword}></input>
                         </div>
                         <button type="submit" className="login-button">Log in</button>
                     </form>
@@ -45,6 +48,22 @@ const Homepage = (props) => {
                     </div>
                     <div className="homepage__image">
                         <img src={image} alt="" className="home-image"/>
+                    </div>
+                </div>
+                <div className="homepage__about">
+                    <div className="homepage__about-howto">
+                        <div className="homepage__icon">
+                            <img src={laptopIcon} alt="" className="laptop"/>
+                            <p className="icon-text">Create<br/>an account</p>
+                        </div>
+                        <div className="homepage__icon">
+                            <img src={codeIcon} alt="" className="code"/>
+                            <p className="icon-text">Write<br/>articles<br/>and share<br/>code</p>   
+                        </div>
+                        <div className="homepage__icon">
+                            <img src={shareIcon} alt="" className="share"/>
+                            <p className="icon-text">Explore<br/>the<br/>{`{ iterator }`}<br/>community</p>   
+                        </div>
                     </div>
                 </div>
             </div>
